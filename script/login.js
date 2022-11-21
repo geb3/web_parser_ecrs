d7d748f701f1bb54807a89e1ce349125440f8e09
Инженер <@cephei.local>
2022-11-21 17:56:40 +0300

    v.0.1

--- script/login.js
+++ script/login.js
@@ -1,12 +1,14 @@
-let jsonData = require('../users/users.json'); 
-console.log(jsonData);
+var sha256 = require('./sha256.js');
 
-// document.querySelector('button').onclick = function () {
-//     const login = document.querySelector("#login").value;
-//     const passwd = document.querySelector("#passwd").value;
-//     console.log(login);
-//     // if (dataUsers[0] == login && dataUsers[1] == passwd) {
-//     //     console.log(login);
-//     //     console.log(passwd);
-//     // }
-// }
+
+document.getElementById("check").onclick = function() {
+    let login = document.getElementById("login").value;
+    let password = document.getElementById("passwd").value;
+
+    if (sha256(login) == "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b" && sha256(password) == "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b") {
+        alert("Yes!")
+    }
+    else alert("Incorrect login or password!");
+}
+
+
